18948afa3936300c1d5229f562c8b9afc0c1048c
Ivan Ernandorena <ivan.ernandorena@gmail.com>
2020-10-03 17:35:57 -0300

    card and cardList components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getMonsters();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters}></CardList>
+      </div>
+    );
+  }
+
+  async getMonsters() {
+    try {
+      const monsters = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const monsters_json = await monsters.json();
+      this.setState({ monsters: monsters_json });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
 
 export default App;
