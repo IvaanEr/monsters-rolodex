d7d9daac3874677fb0f39fd81e61012a0ff8570b
Ivan Ernandorena <ivan.ernandorena@gmail.com>
2020-10-04 11:28:22 -0300

    Updates

---
+++ precache-manifest.5e4ef8017592e46437c22742eeb8eb4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c16defedb48e94cbf49a474b3d53b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "40be79d911b923c7b48f",
+    "url": "/monsters-rolodex/static/css/main.b73c38db.chunk.css"
+  },
+  {
+    "revision": "724ff5f7fbcfae15cd6c",
+    "url": "/monsters-rolodex/static/js/2.1df26df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1df26df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40be79d911b923c7b48f",
+    "url": "/monsters-rolodex/static/js/main.b72cdd0f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
